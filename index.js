2f4d8b976c52f837ae69979b2307d8176cf290c9
lc <1033581609@qq.com>
2020-10-06 17:15:53 +0800

    测试防抖函数功能

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 // 防抖函数的思路是多次触发只执行第一次或者最后一次
-
+/* 
+    参数： 
+        fn： 需要防抖的函数
+        wait： 防抖的时间
+        im： 是立即触发一次还是延迟到最后触发一次
+*/
+function debounce(fn, wait = 500, im = false) {
+    let timer = null;
+    return function (...args) {
+        let now = !timer && im;
+        clearTimeout(timer);
+        timer = setTimeout(() => {
+            clearTimeout(timer);
+            timer = null;
+            !im && fn(...args);
+        }, wait);
+        now && fn(...args);
+    };
+}
